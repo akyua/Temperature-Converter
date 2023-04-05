985501c3affbfa169159014199a351a8588cf10a
Gabriel <gabrielnovatoo@gmail.com>
2023-04-05 09:20:40 -0300

    Converter working

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
-// Forecast today in kelvin saved in the a variable constant
+const celsius = document.getElementById('celsius');
+const fahrenheit = document.getElementById('fahrenheit');
+
+function celToFar(){
+    let output = ( parseFloat(celsius.value) * 9/5) + 32;
+    fahrenheit.value = output.toFixed(2);
+}
+
+function farToCel(){
+    let output = ( parseFloat(fahrenheit.value) - 32 ) / 1.8;
+    celsius.value = output.toFixed(2);
+}
+
+/* // Forecast today in kelvin saved in the a variable constant
 const kelvin = 283;
 // Forecast today converts to celsius
-const celsius = kelvin - 273;
+const celsiust = kelvin - 273;
 // Forecast today converts to fahrenheit
-let fahrenheit = celsius * (9/5) + 32;
+let fahrenheitt = celsiust * (9/5) + 32;
 // Math.floor retire the decimal numbers
-fahrenheit = Math.floor(fahrenheit);
+fahrenheitt = Math.floor(fahrenheitt);
 // Scale Newton
-let newton = celsius * (33/100);
+let newton = celsiust * (33/100);
 // Math.floor retire the decimal numbers
 newton = Math.floor(newton);
 
-console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`)
+console.log(`The temperature is ${fahrenheitt} degrees Fahrenheit.`) */
