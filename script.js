8bf3da156a5b7bb9584bd872dc8eb7a46d1363ef
Gabriel <gabrielnovatoo@gmail.com>
2023-04-05 09:22:38 -0300

    Adjust decimals

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ const fahrenheit = document.getElementById('fahrenheit');
 
 function celToFar(){
     let output = ( parseFloat(celsius.value) * 9/5) + 32;
-    fahrenheit.value = output.toFixed(2);
+    fahrenheit.value = parseFloat(output.toFixed(2));
 }
 
 function farToCel(){
     let output = ( parseFloat(fahrenheit.value) - 32 ) / 1.8;
-    celsius.value = output.toFixed(2);
+    celsius.value = parseFloat(output.toFixed(2));
 }
 
 /* // Forecast today in kelvin saved in the a variable constant
